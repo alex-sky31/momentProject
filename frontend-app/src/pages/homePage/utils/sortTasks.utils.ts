27bb890154ca3a1b28acde4b253349a75945798d
alex-sky31 <alexandre.courbon@epitech.eu>
2024-07-23 12:38:31 +0200

    feat(): add enum for clean

--- frontend-app/src/pages/homePage/utils/sortTasks.utils.ts
+++ frontend-app/src/pages/homePage/utils/sortTasks.utils.ts
@@ -1,15 +1,21 @@
 import { Task } from '../../../@types/Task.type';
 
+enum TaskStatus {
+  TODO = 'todo',
+  IN_PROGRESS = 'inprogress',
+  DONE = 'done'
+}
+
 export const sortedTasksUtils = (
   tasks: Task[]
 ): { todo: Task[]; inprogress: Task[]; done: Task[] } => {
   return tasks.reduce<{ todo: Task[]; inprogress: Task[]; done: Task[] }>(
     (acc, task) => {
-      if (task.status === 'todo') {
+      if (task.status === TaskStatus.TODO) {
         acc.todo.push(task);
-      } else if (task.status === 'inprogress') {
+      } else if (task.status === TaskStatus.IN_PROGRESS) {
         acc.inprogress.push(task);
-      } else if (task.status === 'done') {
+      } else if (task.status === TaskStatus.DONE) {
         acc.done.push(task);
       }
       return acc;
