5f22e5e43251789a7929c6494d779536dc1e7840
alex-sky31 <alexandre.courbon@epitech.eu>
2024-07-24 08:56:29 +0200

    fix(): remove useless controller

--- backend-app/src/modules/users/user.module.ts
+++ backend-app/src/modules/users/user.module.ts
@@ -2,13 +2,12 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { UserService } from './user.service';
-import { UserController } from './user.controller';
 import { PasswordService } from '../password/password.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   providers: [UserService, PasswordService],
-  controllers: [UserController],
+  controllers: [],
   exports: [UserService],
 })
 export class UserModule {}
